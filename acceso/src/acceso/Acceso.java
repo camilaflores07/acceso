4918afd29b56c11b88ccedf792699abbc971df1a
Camila Flores <camila_maria@unitec.edu>
2024-12-14 19:36:43 -0600

    Add files via upload

---
+++ acceso/src/acceso/Acceso.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package acceso;
+
+
+public class Acceso {
+
+   
+    public static void main(String[] args) {
+        login log = new login();
+        log.setVisible(true);
+        
+        
+    }
+    
+}
